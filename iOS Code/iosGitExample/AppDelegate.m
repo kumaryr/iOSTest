19f19260d4407a7e325c4e633184d7d07bcc374e
YRK <user@banpp01.corpzone.internalzone.com>
2015-06-12 14:16:30 +0530

    adding folder, three screen shots

--- iOS Code/iosGitExample/AppDelegate.m
+++ iOS Code/iosGitExample/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // Do your sucking and fucking here.
     return YES;
 }
 
---
+++ iOS Code/iosGitExample/fucing view contrilller.m
@@ -0,0 +1,13 @@
+//
+//  fucing view contrilller.m
+//  iosGitExample
+//
+//  Created by user on 12/06/15.
+//  Copyright (c) 2015 user. All rights reserved.
+//
+
+#import "fucing view contrilller.h"
+
+@implementation fucing_view_contrilller
+
+@end
